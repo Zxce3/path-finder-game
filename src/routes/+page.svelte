80a8a1414f663c57f7eb22d427696481be06c754
Memet Zx <zxce3@gnuweeb.org>
2025-01-16 15:03:30 +0700

    Add timer functionality with start and reset methods in app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,22 @@
   import { onMount } from 'svelte';
   import { app, initializeDOMElements } from '$lib/helper';
 
+  let timer: number = 0;
+  let interval: number;
+
+  function startTimer() {
+    interval = setInterval(() => {
+      timer++;
+      document.getElementById("timer")!.textContent = timer.toString();
+    }, 1000);
+  }
+
+  function resetTimer() {
+    clearInterval(interval);
+    timer = 0;
+    document.getElementById("timer")!.textContent = timer.toString();
+  }
+
   onMount(() => {
     initializeDOMElements();
     app.launchScreen(
@@ -10,11 +26,14 @@
       "Find the nearest yellow dot.",
       "PLAY"
     );
+    app.startTimer = startTimer;
+    app.resetTimer = resetTimer;
   });
 </script>
 
 <div id="app">
   <div id="score"></div>
+  <div id="timer">0</div>
   <svg id="svg"></svg>
   
   <div id="launch-screen" class="is-visible">
