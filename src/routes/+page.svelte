c0554eb45bc45f006f4e003efcc810896d021d7b
Memet Zx <zxce3@gnuweeb.org>
2025-01-16 13:42:12 +0700

    Add launch screen and initialize app elements in main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { app, initializeDOMElements } from '$lib/helper';
+
+  onMount(() => {
+    initializeDOMElements();
+    app.launchScreen(
+      0,
+      "Path finder",
+      "Find the nearest yellow dot.",
+      "PLAY"
+    );
+  });
+</script>
+
+<div id="app">
+  <div id="score"></div>
+  <svg id="svg"></svg>
+  
+  <div id="launch-screen" class="is-visible">
+    <div id="launch-screen-content">
+      <h1 id="launch-screen__title">s</h1>
+      <p id="launch-screen__description"></p>
+      <button class="btn" id="start-btn">PLAY</button>
+    </div>
+  </div>
+</div>
