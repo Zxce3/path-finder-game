7d45a62aab5e010084a5f4b5c2fa3c2cfb6f78a9
Memet Zx <zxce3@gnuweeb.org>
2025-02-08 16:38:04 +0700

    Add time limit store with a default of 30 seconds

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -5,3 +5,4 @@ export const isPaused = writable(false);
 export const highScore = writable(0);
 export const score = writable(0);
 export const level = writable(1);
+export const timeLimit = writable(30); // 30 seconds time limit
